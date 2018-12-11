ccab72c98c84bbf8f27a192b2b57251b5c0fe410
h1d4nz <35686292+h1d4nz@users.noreply.github.com>
2018-12-11 23:39:21 +0300

    Add files via upload

---
+++ xor.py
@@ -0,0 +1,17 @@
+inputFile = open('input.txt','r')
+outputFile = open('output.txt','w')
+
+print ('Enter the key: ')
+key = raw_input()
+
+outputFile.write('Using key: {}\n'.format(key))
+
+for line in inputFile:
+	for ch in line:
+		try:
+			outputFile.write('{}\n'.format(ord(ch) ^ ord(key)))
+		except TypeError:
+			outputFile.write('{}\n'.format(ord(ch) ^ int(key)))
+
+inputFile.close()
+outputFile.close()
